77d62dbdd4b25fc4dc036d9d0034d3c75954e96e
sagart <sagart@gmail.com>
2024-06-20 16:37:55 +0530

    june20_01

--- lib/utils/project_utils.dart
+++ lib/utils/project_utils.dart
@@ -5,6 +5,7 @@ class ProjectUtils {
   final String? androidLink;
   final String? iosLink;
   final String? webLink;
+  final String? github;
 
   ProjectUtils({
     required this.image,
@@ -13,6 +14,7 @@ class ProjectUtils {
     this.androidLink,
     this.iosLink,
     this.webLink,
+    this.github,
   });
 }
 
@@ -25,16 +27,16 @@ List<ProjectUtils> profProjectUtils = [
       subtitle: "App to manage your restaurant business",
       webLink: "https://cafe-app-01.web.app/",
       image: 'assets/projects/1.png'),
-  // ProjectUtils(
-  //     title: "Cafe App",
-  //     subtitle: "App to manage your restaurant business",
-  //     webLink: "https://cafe-app-01.web.app/",
-  //     image: 'assets/projects/1.png'),
-  // ProjectUtils(
-  //     title: "Cafe App",
-  //     subtitle: "App to manage your restaurant business",
-  //     webLink: "https://cafe-app-01.web.app/",
-  //     image: 'assets/projects/1.png'),
+  ProjectUtils(
+      title: "Event Management App",
+      subtitle: "App to manage your Event Seamlessly",
+      webLink: "https://cafe-app-01.web.app/",
+      image: 'assets/projects/efi.png'),
+  ProjectUtils(
+      title: "Wordpress - Flutter RESTApi App",
+      subtitle: "App to connect your wordpress backend to flutter",
+      webLink: "https://cafe-app-01.web.app/",
+      image: 'assets/projects/Wp1.png'),
   // ProjectUtils(
   //     title: "Cafe App",
   //     subtitle: "App to manage your restaurant business",
@@ -46,23 +48,23 @@ List<ProjectUtils> profProjectUtils = [
 // PRACTICE PROJECTS
 
 List<ProjectUtils> workProjectUtils = [
-  //  ProjectUtils(
-  //   title: "p App",
-  //   subtitle: "App to manage your restaurant business",
-  //   webLink: "https://cafe-app-01.web.app/",
-  //   image: 'assets/projects/1.png'),
+  ProjectUtils(
+      title: "Laravel - Flutter Communication",
+      subtitle: "Connection Laravel backend to Flutter Frontend",
+      webLink: "https://cafe-app-01.web.app/",
+      image: 'assets/projects/Laravel.png'),
 
-  // ProjectUtils(
-  //   title: "p App",
-  //   subtitle: "App to manage your restaurant business",
-  //   webLink: "https://cafe-app-01.web.app/",
-  //   image: 'assets/projects/1.png'),
+  ProjectUtils(
+      title: "WhatsApp Flutter UI",
+      subtitle: "Clone of WhatsApp UI",
+      webLink: "https://cafe-app-01.web.app/",
+      image: 'assets/projects/WA1.png'),
 
-  // ProjectUtils(
-  //   title: "p App",
-  //   subtitle: "App to manage your restaurant business",
-  //   webLink: "https://cafe-app-01.web.app/",
-  //   image: 'assets/projects/1.png'),
+  ProjectUtils(
+      title: "Maze Runner Game",
+      subtitle: "Maze Runner Game Made using flutter package",
+      webLink: "https://cafe-app-01.web.app/",
+      image: 'assets/projects/Maze.png'),
 
   // ProjectUtils(
   //   title: "p App",
